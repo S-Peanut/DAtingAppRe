fe07cc6df7fb19a88a00dc19a563116a670a2c8f
Silviu <silvianpascaru@gmail.com>
2023-10-27 09:15:31 +0300

    End Section 10 -update resources

--- Client/src/app/members/member-detail/member-detail.component.ts
+++ Client/src/app/members/member-detail/member-detail.component.ts
@@ -24,10 +24,10 @@ export class MemberDetailComponent implements OnInit {
      ngOnInit(): void {
           this.loadMember();
      }
-      
+     
      loadMember() {
           const username = this.route.snapshot.paramMap.get('username');
-
+          
           if (!username) return;
           this.memberService.getMember(username).subscribe({
                next: member => {
